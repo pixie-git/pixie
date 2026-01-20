03eddb55d7cbb68c74152c08cb2529c5a90fd74a
Matteo Susca <matteo.susca@studio.unibo.it>
2026-01-20 14:32:24 +0100

    PIX-17 palette with white as 0 (default)

--- client/src/config/defaultPalette.ts
+++ client/src/config/defaultPalette.ts
@@ -1,18 +1,18 @@
 export const defaultPalette: string[] = [
-  '#000000', // 0: Nero
-  '#1D2B53', // 1: Blu Notte
-  '#7E2553', // 2: Vinaccia
-  '#008751', // 3: Verde Muschio
-  '#AB5236', // 4: Marrone
-  '#5F574F', // 5: Grigio Scuro
-  '#C2C3C7', // 6: Grigio Chiaro
-  '#FFF1E8', // 7: Bianco Panna
-  '#FF004D', // 8: Rosso
-  '#FFA300', // 9: Arancione
-  '#FFEC27', // 10: Giallo
-  '#00E436', // 11: Verde Lime
-  '#29ADFF', // 12: Blu Cielo
-  '#83769C', // 13: Indaco
-  '#FF77A8', // 14: Rosa
-  '#FFCCAA', // 15: Pesca
+  '#FFFFFF', // 0: Bianco
+  '#000000', // 1: Nero
+  '#1D2B53', // 2: Blu Notte
+  '#7E2553', // 3: Vinaccia
+  '#008751', // 4: Verde Muschio
+  '#AB5236', // 5: Marrone
+  '#5F574F', // 6: Grigio Scuro
+  '#C2C3C7', // 7: Grigio Chiaro
+  '#FFF1E8', // 8: Bianco Panna
+  '#FF004D', // 9: Rosso
+  '#FFA300', // 10: Arancione
+  '#FFEC27', // 11: Giallo
+  '#00E436', // 12: Verde Lime
+  '#29ADFF', // 13: Blu Cielo
+  '#83769C', // 14: Indaco
+  '#FF77A8', // 15: Rosa
 ];
