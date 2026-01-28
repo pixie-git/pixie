d69498070cabcf66df643186683aef9c3592fd67
Andrea Zavatta <zavattaandrea@gmail.com>
2026-01-28 17:40:49 +0100

    PIX-54: fix bug on download icon

--- client/src/views/PlayView.vue
+++ client/src/views/PlayView.vue
@@ -65,7 +65,7 @@ onMounted(async () => {
         
         <!-- Export Button -->
         <button class="export-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 0 0 1-2 2H5a2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
           Export
         </button>
 
